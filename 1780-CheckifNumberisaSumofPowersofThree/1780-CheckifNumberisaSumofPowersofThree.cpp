ccd6fa47044de59aef8493618f38243e349e941c
saurabh9112 <106989793+saurabh9112@users.noreply.github.com>
2025-08-13 17:31:01 +0530

    File updated at 8/13/2025, 5:31:01 PM

--- 1780-CheckifNumberisaSumofPowersofThree/1780-CheckifNumberisaSumofPowersofThree.cpp
+++ 1780-CheckifNumberisaSumofPowersofThree/1780-CheckifNumberisaSumofPowersofThree.cpp
@@ -1,21 +1,28 @@
-// Last updated: 8/13/2025, 5:19:18 PM
+// Last updated: 8/13/2025, 5:31:01 PM
 class Solution {
 public:
-    bool solve(int n,vector<int> powers,int index,int sum){
+    bool solve(int n,vector<int> powers,int index,int sum,vector<vector<int>> &dp){
         if(index >= powers.size()){
             return sum == n;
         }
         if(sum == n) return true;
         if(sum > n) return false;
-        return solve(n,powers,index+1,sum+powers[index]) || solve(n,powers,index+1,sum);
+        if(dp[index][sum] != -1) return dp[index][sum];
+        return dp[index][sum] = solve(n,powers,index+1,sum+powers[index],dp) || solve(n,powers,index+1,sum,dp);
     }
     bool checkPowersOfThree(int n) {
-        vector<int> powers;
-        int i = 0;
-        while(pow(3,i) <= n){
-            powers.push_back(pow(3,i));
-            i++;
+        // vector<int> powers;
+        // int i = 0;
+        // while(pow(3,i) <= n){
+        //     powers.push_back(pow(3,i));
+        //     i++;
+        // }
+        // vector<vector<int>> dp(powers.size(),vector<int> (n+1,-1));
+        // return solve(n,powers,0,0,dp);
+        while (n > 0) {
+            if (n % 3 == 2) return false; // found a '2' in base 3
+            n /= 3;
         }
-        return solve(n,powers,0,0);
+        return true;
     }
 };
