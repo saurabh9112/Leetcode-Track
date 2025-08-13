d1a6c5659d4d66a97d20ee0ea43553736b19da2d
saurabh9112 <106989793+saurabh9112@users.noreply.github.com>
2025-08-13 19:18:16 +0530

    File updated at 8/13/2025, 7:18:15 PM

--- 18-4Sum/18-4Sum.cpp
+++ 18-4Sum/18-4Sum.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/13/2025, 7:01:07 PM
+// Last updated: 8/13/2025, 7:18:15 PM
 class Solution {
 public:
     void solve(vector<int>& nums, int index, long long target, vector<int>& currArr, vector<vector<int>>& ans, int k) {
@@ -19,9 +19,44 @@ public:
 
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        vector<int> currArr;
-        solve(nums, 0, (long long)target, currArr, ans, 4);
-        return ans;
+        // vector<vector<int>> ans;
+        // vector<int> currArr;
+        // solve(nums, 0, (long long)target, currArr, ans, 4);
+        // return ans;
+        set<vector<int>> ans;
+        int size = nums.size();
+        for(int i=0;i<size-3;i++){
+            for(int j=i+1;j<size-2;j++){
+                int left = j+1;
+                int right = size-1;
+                long long a = nums[i],b = nums[j];
+                long long diff = target - (a+b);
+                while(left < right && left < size && right >= 0){
+                    long long add = nums[left] + nums[right];
+                    if(add == diff){
+                        vector<int> arr;
+                        arr.push_back(a);
+                        arr.push_back(b);
+                        arr.push_back(nums[left]);
+                        arr.push_back(nums[right]);
+                        ans.insert(arr);
+                        right--;
+                        left++;
+                    }
+                    else if(add > diff){
+                        right--;
+                    }
+                    else{
+                        left++;
+                    }
+                }
+            }
+        }
+
+        vector<vector<int>> result;
+        for(auto arr:ans){
+            result.push_back(arr);
+        }
+        return result;
     }
 };
