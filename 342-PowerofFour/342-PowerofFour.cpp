c779d3899112a0dda88bdc67c519767633cf1e16
saurabh9112 <106989793+saurabh9112@users.noreply.github.com>
2025-08-15 12:22:51 +0530

    File updated at 8/15/2025, 12:22:52 PM

--- 342-PowerofFour/342-PowerofFour.cpp
+++ 342-PowerofFour/342-PowerofFour.cpp
@@ -1,15 +1,9 @@
-// Last updated: 8/15/2025, 12:22:14 PM
+// Last updated: 8/15/2025, 12:22:52 PM
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n <= 0) return 0;
-        while(n > 0){
-            if(n == 1) return 1;
-            if(n%4 == 3) return 0;
-            if(n%4 == 2) return 0;
-            if(n%4 == 1) return 0;
-            n /= 4;
-        }
-        return true;
+        if (n <= 0) return false;
+        double div = log(n) / log(4);
+        return fabs(div - round(div)) < 1e-10;
     }
 };
