9f0816026a8cd5c2c9630e2436110545edda794c
saurabh9112 <106989793+saurabh9112@users.noreply.github.com>
2025-08-17 17:34:28 +0530

    Create 837-New21Game

---
+++ 837-New21Game/837-New21Game.cpp
@@ -0,0 +1,25 @@
+// Last updated: 8/17/2025, 5:34:27 PM
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if (k == 0 || n >= k + maxPts - 1) return 1.0;
+        
+        vector<double> dp(n + 1, 0.0);
+        dp[0] = 1.0;
+        double windowSum = 1.0, result = 0.0;
+        
+        for (int i = 1; i <= n; i++) {
+            dp[i] = windowSum / maxPts;
+            if (i < k) {
+                windowSum += dp[i];
+            } else {
+                result += dp[i];
+            }
+            if (i - maxPts >= 0) {
+                windowSum -= dp[i - maxPts];
+            }
+        }
+        
+        return result;
+    }
+};
