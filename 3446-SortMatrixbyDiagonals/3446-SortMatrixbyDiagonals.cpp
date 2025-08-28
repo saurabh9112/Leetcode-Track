2bba8d66902a59c6a29a79de8dc822e2ab277255
saurabh9112 <106989793+saurabh9112@users.noreply.github.com>
2025-08-28 21:12:34 +0530

    Create 3446-SortMatrixbyDiagonals

---
+++ 3446-SortMatrixbyDiagonals/3446-SortMatrixbyDiagonals.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/28/2025, 9:12:34 PM
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        for (int i = 0; i < n; i++) {
+            vector<int> tmp;
+            for (int j = 0; i + j < n; j++) {
+                tmp.push_back(grid[i + j][j]);
+            }
+            sort(tmp.begin(), tmp.end(), greater<int>());
+            for (int j = 0; i + j < n; j++) {
+                grid[i + j][j] = tmp[j];
+            }
+        }
+        for (int j = 1; j < n; j++) {
+            vector<int> tmp;
+            for (int i = 0; j + i < n; i++) {
+                tmp.push_back(grid[i][j + i]);
+            }
+            sort(tmp.begin(), tmp.end());
+            for (int i = 0; j + i < n; i++) {
+                grid[i][j + i] = tmp[i];
+            }
+        }
+        return grid;
+    }
+};
