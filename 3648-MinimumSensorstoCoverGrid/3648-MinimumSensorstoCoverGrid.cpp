ebd124b8a99e45926a23f1cb86641c53a2ebbdc5
saurabh9112 <106989793+saurabh9112@users.noreply.github.com>
2025-09-11 17:17:43 +0530

    Create 3648-MinimumSensorstoCoverGrid

---
+++ 3648-MinimumSensorstoCoverGrid/3648-MinimumSensorstoCoverGrid.cpp
@@ -0,0 +1,10 @@
+// Last updated: 9/11/2025, 5:17:42 PM
+class Solution {
+public:
+    int minSensors(int n, int m, int k) {
+        int side = 2 * k + 1; // coverage in one dimension
+        long long rows = (n + side - 1) / side;
+        long long cols = (m + side - 1) / side;
+        return (int)(rows * cols);
+    }
+};
