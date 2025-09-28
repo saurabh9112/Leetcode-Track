37fcb9a9bb084f32e211155026ebe32e40572e88
saurabh9112 <106989793+saurabh9112@users.noreply.github.com>
2025-09-28 21:44:33 +0530

    Create 812-LargestTriangleArea

---
+++ 812-LargestTriangleArea/812-LargestTriangleArea.cpp
@@ -0,0 +1,26 @@
+// Last updated: 9/28/2025, 9:44:34 PM
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        int N = points.size();
+        double ans = 0;
+        for (int i = 0; i < N; ++i) {
+            for (int j = i + 1; j < N; ++j) {
+                for (int k = j + 1; k < N; ++k) {
+                    ans = max(ans, area(points[i], points[j], points[k]));
+                }
+            }
+        }
+        return ans;
+    }
+
+    double area(vector<int>& P, vector<int>& Q, vector<int>& R) {
+        return 0.5 * fabs(P[0]*Q[1] + Q[0]*R[1] + R[0]*P[1]
+                         - P[1]*Q[0] - Q[1]*R[0] - R[1]*P[0]);
+    }
+};
