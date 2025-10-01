67c18119ec2c138232281a02e14cb7914c068b01
saurabh9112 <106989793+saurabh9112@users.noreply.github.com>
2025-10-01 23:49:10 +0530

    Create 3697-ComputeDecimalRepresentation

---
+++ 3697-ComputeDecimalRepresentation/3697-ComputeDecimalRepresentation.cpp
@@ -0,0 +1,19 @@
+// Last updated: 10/1/2025, 11:49:09 PM
+class Solution {
+public:
+    vector<int> decimalRepresentation(int n) {
+        vector<int> res;
+        long long temp = n;
+        long long mul = 1;
+        while (temp) {
+            int digit = temp % 10;
+            if (digit != 0) {
+                res.push_back((int)(1LL*digit*mul));
+            }
+            mul *= 10;
+            temp /= 10;
+        }
+        sort(res.begin(),res.end(),greater<int>());
+        return res;
+    }
+};
